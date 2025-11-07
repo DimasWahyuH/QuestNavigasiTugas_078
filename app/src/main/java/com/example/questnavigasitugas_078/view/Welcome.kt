2f28b212e77df16706516b6ae7c32e45532a0324
dimas <dimaswh19@gmail.com>
2025-11-07 10:51:23 +0700

    Menambahkan Halaman Welcome

---
+++ app/src/main/java/com/example/questnavigasitugas_078/view/Welcome.kt
@@ -0,0 +1,26 @@
+package com.example.questnavigasitugas_078.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.questnavigasitugas_078.R
+
+@Composable
+fun Welcome(navController: NavHostController) {
