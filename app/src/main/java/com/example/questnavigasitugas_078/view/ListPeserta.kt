d18231d4d1f69803f9b133fdfdd7e3ea2f88492b
dimas <dimaswh19@gmail.com>
2025-11-07 10:57:57 +0700

    Menambahkan halaman ListPeserta

---
+++ app/src/main/java/com/example/questnavigasitugas_078/view/ListPeserta.kt
@@ -0,0 +1,8 @@
+package com.example.questnavigasitugas_078.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ListPeserta(navController: NavHostController, viewModel: Any)
+}
