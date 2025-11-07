9d8b55ab103fad33b2200074432be6a6045bc040
dimas <dimaswh19@gmail.com>
2025-11-07 11:21:23 +0700

    Menambahkan Surface dan Column

--- app/src/main/java/com/example/questnavigasitugas_078/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_078/view/ListPeserta.kt
@@ -1,8 +1,41 @@
 package com.example.questnavigasitugas_078.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.questnavigasitugas_078.R
 
 @Composable
-fun ListPeserta(navController: NavHostController, viewModel: Any)
-}
+fun ListPeserta(navController: NavHostController, viewModel: Any){
+    val peserta = stringArrayResource(id = R.array.data_peserta)
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color.White, Color.LightGray)
+    )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
