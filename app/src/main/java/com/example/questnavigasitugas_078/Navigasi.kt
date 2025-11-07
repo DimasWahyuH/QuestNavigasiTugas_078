da2a99a04f683e16df085679337f1470063dcbce
dimas <dimaswh19@gmail.com>
2025-11-07 10:24:37 +0700

    Menambahkan enum class Kelola Halaman

---
+++ app/src/main/java/com/example/questnavigasitugas_078/Navigasi.kt
@@ -0,0 +1,15 @@
+package com.example.questnavigasitugas_078
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class KelolaHalaman {
+    Welcome,
+    ListPeserta,
+    Formulir
+}
+
