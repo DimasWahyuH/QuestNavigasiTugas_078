e3eb15484c3ec4d26658b0bf1a22b337c8b9ef4e
dimas <dimaswh19@gmail.com>
2025-11-07 21:59:54 +0700

    Menerapkan ViewModel dan composable ke NavHost

--- app/src/main/java/com/example/questnavigasitugas_078/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_078/Navigasi.kt
@@ -1,11 +1,15 @@
 package com.example.questnavigasitugas_078
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_078.view.Formulir
+import com.example.questnavigasitugas_078.view.ListPeserta
+import com.example.questnavigasitugas_078.view.Welcome
 
 enum class KelolaHalaman {
     Welcome,
@@ -23,13 +27,13 @@ fun DataApp(
         startDestination = KelolaHalaman.Welcome.name
     ){
         composable(route = KelolaHalaman.Welcome.name) {
-
+            Welcome(navController=navController)
         }
         composable(route = KelolaHalaman.ListPeserta.name) {
-
+            ListPeserta(navController=navController, viewModel ())
         }
         composable(route = KelolaHalaman.Formulir.name) {
-
+            Formulir(navController=navController, viewModel ())
         }
     }
 }
